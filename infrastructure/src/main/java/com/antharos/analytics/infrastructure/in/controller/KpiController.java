14714a935b75cf5f0dafe76b9645042999ecb1e9
Miguel García de Prada <miguelgardep@uoc.edu>
2025-05-06 20:49:50 +0200

    #6 Implement authentication and authorization

--- infrastructure/src/main/java/com/antharos/analytics/infrastructure/in/controller/KpiController.java
+++ infrastructure/src/main/java/com/antharos/analytics/infrastructure/in/controller/KpiController.java
@@ -3,6 +3,8 @@ package com.antharos.analytics.infrastructure.in.controller;
 import com.antharos.analytics.domain.*;
 import com.antharos.analytics.domain.service.ReportService;
 import java.util.List;
+
+import com.antharos.analytics.infrastructure.security.ManagementOnly;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,26 +17,31 @@ public class KpiController {
 
   private final ReportService reportService;
 
+  @ManagementOnly
   @GetMapping("/employees/monthly")
   public List<MonthlyEmployeeCount> getEmployeesByMonth() {
     return reportService.getEmployeeCountByMonth();
   }
 
+  @ManagementOnly
   @GetMapping("/salary/monthly")
   public List<MonthlySalaryCost> getSalaryByMonth() {
     return reportService.getSalaryCostByMonth();
   }
 
+  @ManagementOnly
   @GetMapping("/employees/department")
   public List<DepartmentEmployees> getEmployeesByDepartment() {
     return reportService.getEmployeesByDepartment();
   }
 
+  @ManagementOnly
   @GetMapping("/salary/department")
   public List<DepartmentSalary> getSalaryByDepartment() {
     return reportService.getSalaryByDepartment();
   }
 
+  @ManagementOnly
   @GetMapping("/employees/job-title")
   public List<JobTitleEmployees> getEmployeesByJobTitle() {
     return reportService.getEmployeesByJobTitle();
